eb137a4ce8125a18fb8b012b77f4c7a0681b17aa
akulkarnihawk1995 <akulkarni10@hawk.iit.edu>
2020-06-13 01:48:09 -0500

    Add AboutActivity

--- app/src/main/java/com/akshay/know_your_government_shdh/AboutActivity.java
+++ app/src/main/java/com/akshay/know_your_government_shdh/AboutActivity.java
@@ -2,7 +2,10 @@ package com.akshay.know_your_government_shdh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    public void apiClicked(View v)
+    {
+        String dem_URL = "https://developers.google.com/civic-information/";
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(dem_URL));
+        startActivity(i);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
